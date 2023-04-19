7a98b31972a3923576b4a2883f4d38104ffac44d
nineonine <mail4chemik@gmail.com>
2023-04-19 15:30:40 -0700

    tui template app code

---
+++ src/ui.rs
@@ -0,0 +1,32 @@
+use ratatui::{
+    backend::Backend,
+    layout::Alignment,
+    style::{Color, Style},
+    widgets::{Block, BorderType, Borders, Paragraph},
+    Frame,
+};
+
+use crate::app::App;
+
+/// Renders the user interface widgets.
+pub fn render<B: Backend>(_app: &mut App, frame: &mut Frame<'_, B>) {
+    // This is where you add new widgets.
+    // See the following resources:
+    // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
+    // - https://github.com/tui-rs-revival/ratatui/tree/master/examples
+    frame.render_widget(
+        Paragraph::new(format!(
+            "Press `Esc`, `Ctrl-C` or `q` to quit."
+        ))
+        .block(
+            Block::default()
+                .title("Garbage Collection Visualization Environment")
+                .title_alignment(Alignment::Center)
+                .borders(Borders::ALL)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Black))
+        .alignment(Alignment::Center),
+        frame.size(),
+    )
+}
