e5940ea52a84aada17faa7ef135b6b330fb2f5bb
nineonine <mail4chemik@gmail.com>
2023-10-29 22:24:52 -0700

    fix formatting issue

--- src/gc/compact/two_finger.rs
+++ src/gc/compact/two_finger.rs
@@ -102,7 +102,7 @@ fn can_fit_into(heap: &Heap, move_in: ObjAddr, dest: ObjAddr) -> bool {
 fn move_object(heap: &mut Heap, eventlog: &mut Vec<GCEvent>, from: ObjAddr, to: ObjAddr) {
     if heap.objects.get(&from).is_some() {
         let size = heap.objects.get(&from).unwrap().size();
-        eventlog.push(GCEvent::MoveObject{from, to, size});
+        eventlog.push(GCEvent::MoveObject { from, to, size });
         match heap.move_object(from, to) {
             Ok(res) => res,
             Err(_e) => panic!("move_object {_e}"),
